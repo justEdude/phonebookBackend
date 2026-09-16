e0a8f62c27cc9e7459a8aedd2c3a63835b3b2d81
Danton Pretti <Dantonprettibcs@hotmail.com>
2026-09-16 09:56:21 -0400

    uibuild

--- index.js
+++ index.js
@@ -59,8 +59,12 @@ app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :d
 }))
 //app.use(requestLogger)
 
-//gets contacts
 app.get('/', (request, response)=>{
+  response.send(`<h1>hello world</h1>`)
+})
+
+//gets contacts
+app.get('/api/persons', (request, response)=>{
   response.json(contacts)
 })
 //finds a specific contact
